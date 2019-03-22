ee4d0e414fd533bd4105f854cd3e05fdc1509c02
Facu G <fgerstner@devartis.com>
2019-03-22 10:37:43 -0300

    add example

---
+++ example/App.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import SpringPullToRefresh from "react-native-spring-pull-to-refresh";
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+        <SpringPullToRefresh
+            initialText={"Pull Me!"}
+            releaseText={"Release Me!"}
+            onRefresh={() => {}}
+            refreshing={false}
+            data={[]}
+            renderItem={() => {}}
+            keyExtractor={() => {}}
+            height={50}
+            maxHeight={150}
+            backgroundColor={"#000000"}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
