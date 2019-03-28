adb3037a44572ffbd2ffb697f99be54463a7ba6e
Facu G <fgerstner@devartis.com>
2019-03-28 16:19:53 -0300

    example fixes

--- example/App.js
+++ example/App.js
@@ -6,55 +6,49 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
+import React, {Component} from "react";
+import {StyleSheet, View, Button} from "react-native";
 import SpringPullToRefresh from "react-native-spring-pull-to-refresh";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <SpringPullToRefresh
-            initialText={"Pull Me!"}
-            releaseText={"Release Me!"}
-            onRefresh={() => {}}
-            refreshing={false}
-            data={[]}
-            renderItem={() => {}}
-            keyExtractor={() => {}}
-            height={50}
-            maxHeight={150}
-            backgroundColor={"#000000"}
-            draggingSpeed={30}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <SpringPullToRefresh
+                    initialText={"Pull Me!"}
+                    releaseText={"Release Me!"}
+                    onRefresh={() => {}}
+                    refreshing={false}
+                    data={[{id: 1}, {id: 2}]}
+                    renderItem={({item}) => <Button title={`item-${item.id}`} onPress={() => console.log("pressed!")}/>}
+                    keyExtractor={item => item.id.toString()}
+                    height={50}
+                    maxHeight={150}
+                    backgroundColor={"#000000"}
+                    draggingSpeed={30}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#0e36ff",
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+    },
+    instructions: {
+        textAlign: "center",
+        color: "#333333",
+        marginBottom: 5,
+    },
 });
